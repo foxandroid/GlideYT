0e0a900582c573f6d5d5fb83edefb4c113bc575e
foxandroid <foxstream001@gmail.com>
2022-07-09 22:23:10 +0530

    Initial commit

---
+++ app/src/main/java/com/example/glideyt/MainActivity.kt
@@ -0,0 +1,34 @@
+package com.example.glideyt
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.engine.DiskCacheStrategy
+import com.example.glideyt.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.button.setOnClickListener {
+
+            val url = "https://picsum.photos/300"
+
+            Glide.with(this)
+                .load(url)
+                .fitCenter()
+                .skipMemoryCache(true)
+                .diskCacheStrategy(DiskCacheStrategy.NONE)
+                .placeholder(R.drawable.placeholder)
+                .into(binding.imageView)
+
+
+        }
+
+    }
+}
